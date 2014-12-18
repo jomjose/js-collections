1ef07f7a9b36a1023a4b64a37c4ab81a6a63c0ce
Jom Jose <jomjose.engr@gmail.com>
2014-12-18 23:55:16 +0530

    get current query string

---
+++ url/getCurrentQueryString.js
@@ -0,0 +1,8 @@
+/**
+ * getCurrentQueryString
+ *
+ * @return {String} e.g. "foo=bar&fizz=buzz" (no ? symbol)
+ */
+function getCurrentQueryString() {
+    return window.location.search.replace(/^\?/, '');
+}
