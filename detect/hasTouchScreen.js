9425db41a69d92906d136b6181496fccab14d590
Jom Jose <jomjose.engr@gmail.com>
2014-12-21 23:52:20 +0530

    has touch screen

---
+++ detect/hasTouchScreen.js
@@ -0,0 +1,8 @@
+/**
+ * hasTouchScreen
+ *
+ * @return {Boolean} true or false
+ */
+function hasTouchScreen() {
+    return ('ontouchstart' in window || window.DocumentTouch && document instanceof DocumentTouch) ? !0 : !1;
+}
